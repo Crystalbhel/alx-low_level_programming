154159464628751f1e82d3ff0b03a83e5140fec4
Crystalbhel <onwuemedaniella@gmail.com>
2023-02-16 20:30:23 -0800

    hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main functions
+ *
+ * Return: always 0
+ */
+
+init main(void)
+
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
