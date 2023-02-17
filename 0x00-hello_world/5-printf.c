20892e6443d16862b6b285c412d2686d1b3b8228
Crystalbhel <onwuemedaniella@gmail.com>
2023-02-16 21:49:54 -0800

    werror

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main funtion
+ * Return: always return 0
+ */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
