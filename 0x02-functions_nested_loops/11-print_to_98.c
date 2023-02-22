239ba07063dfb898cc9dbc896a81c4863cb10cb2
Crystalbhel <onwuemedaniella@gmail.com>
2023-02-22 12:13:36 -0800

    battery street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_to_98 - function to print natural numbers
  * @n: function parameter
  * Return: return always 0
  */
-
 void print_to_98(int n)
 {
 	int i;
@@ -15,26 +13,32 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+				printf("%d ,", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-		printf("\n");
 	}
 	else if (n < 0)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+				printf("%d ,", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-		printf("\n");
 	}
 	else if (n > 98)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+				printf("%d ,", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-		printf("\n");
 	}
 	else if (n == 98)
-		printf("%d, ", n);
+		printf("%d", n);
 }
