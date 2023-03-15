380fd059b8c63eaedc2dc8111f9c0f14f4168d59
Crystalbhel <onwuemedaniella@gmail.com>
2023-03-15 10:14:22 -0700

    better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,25 +9,30 @@
  */
 
 int **alloc_grid(int width, int height)
+
 {
 	int **mee;
-	int x, y
-		if (width <= 0 || height <= 0)
-			return (NULL);
+	int x, y;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
 	mee = malloc(sizeof(int *) * height);
+
 	if (mee == NULL)
 
 		return (NULL);
 
 	for (x = 0; x < height; x++)
-
 	{
 		mee[x] = malloc(sizeof(int) * width);
+
 		if (mee[x] == NULL)
 		{
 			for (; x >= 0; x--)
 				free(mee[x]);
 			free(mee);
+
 			return (NULL);
 		}
 	}
@@ -37,5 +42,6 @@ int **alloc_grid(int width, int height)
 		for (y = 0; y < width; y++)
 			mee[x][y] = 0;
 	}
+
 	return (mee);
 }
